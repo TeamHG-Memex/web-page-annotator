47b4ac7ff7f742545ee8a1d74039233b1ee54ba4
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-08-15 14:33:38 +0300

    Add some buttons to get the sense of UI

--- static/js/parent.js
+++ static/js/parent.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
     console.log('hi from parent');
+
+    var $childPage = $('iframe#child-page');
+    var $window = $(window);
+
+    $childPage.width($window.width());
+    $childPage.height($window.height() - $childPage.offset().top);
+
     $(document.body).on('label-element', function (event) {
         console.log('event in parent', event);
     });
