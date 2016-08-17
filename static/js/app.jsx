1f007f747be6e5ca99d5549fc3fabd265b47fcdd
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-08-17 16:55:32 +0300

    Urls import "dialog"

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -2,50 +2,67 @@ var App = React.createClass({
     getInitialState: function () {
         return {
             idx: 0,
+            urls: [],
+            importingUrls: false,
             labelAt: null,
             labeled: {}  // url -> selector -> labelData
         };
     },
     render: function () {
-        console.log('App.render', this.state);
         var url = this.currentUrl();
-        var labelDropdown;
-        if (this.state.labelAt) {
-            var urlLabeled = this.state.labeled[url];
-            var labelValue;
-            if (urlLabeled) {
-                labelValue = urlLabeled[this.state.labelAt.selector];
-            }
-            labelDropdown = <LabelDropdown
-                labels={ this.props.labels }
-                position={ this.state.labelAt }
-                value={ labelValue }
-                onLabelFinishEdit={ this.onLabelFinishEdit }
+        var labelDropdown, iframe, urlsImport;
+        if (url) {
+            if (this.state.labelAt) {
+                var urlLabeled = this.state.labeled[url];
+                var labelValue;
+                if (urlLabeled) {
+                    labelValue = urlLabeled[this.state.labelAt.selector];
+                }
+                labelDropdown = <LabelDropdown
+                    labels={ this.props.labels }
+                    position={ this.state.labelAt }
+                    value={ labelValue }
+                    onLabelFinishEdit={ this.onLabelFinishEdit }
                 />
+            }
+            var labeled = this.state.labeled[url];
+            iframe = <IFrame url={ url } labeled={ labeled }/>
         }
-        var btnClasses = 'waves-effect waves-light btn';
-        var labeled = this.state.labeled[url];
+        if (this.state.importingUrls) {
+            urlsImport = <UrlsImport
+                urls={ this.state.urls }
+                cancelImportUrls={ this.cancelImportUrls }
+                doImportUrls={ this.doImportUrls }/>;
+        }
+        var btnClasses = function (enabled) {
+            return 'waves-effect waves-light btn' + (enabled ? '' : ' disabled');
+        };
         return <div>
             <div id="controls">
-                <a className={ btnClasses } onClick={ this.onReload }>reload</a>{' '}
-                <a className={ btnClasses + (this.previousEnabled() ? '' : ' disabled') }
+                <a className={ btnClasses(this.reloadEnabled()) }
+                   onClick={ this.onReload }>reload</a>{' '}
+                <a className={ btnClasses(this.previousEnabled()) }
                    onClick={ this.onPrevious }>
                     <i className="material-icons left">skip_previous</i>prev
                 </a>{' '}
                 <div className="chip url-chip">{ url || '-' }</div>{' '}
-                <a className={ btnClasses + (this.nextEnabled() ? '' : ' disabled') }
+                <a className={ btnClasses(this.nextEnabled()) }
                    onClick={ this.onNext }>
                     <i className="material-icons right">skip_next</i>next
                 </a>{' '}
-                <a className={ btnClasses + ' disabled' }>import urls</a>{' '}
-                <a className={ btnClasses + ' disabled' }>export pages & labels</a>{' '}
+                <a className={ btnClasses(true) }
+                    onClick={ this.onImportUrls }>import urls</a>{' '}
+                <a className={ btnClasses(this.exportEnabled()) }>export pages & labels</a>{' '}
             </div>
-            <IFrame url={ url } labeled={ labeled }/>
+            { iframe }
             { labelDropdown }
+            { urlsImport }
         </div>;
     },
     currentUrl: function () {
-        return this.props.urls[this.state.idx];
+        if (this.state.urls.length > 0) {
+            return this.state.urls[this.state.idx];
+        }
     },
     onPrevious: function (event) {
         if (this.previousEnabled()) {
@@ -62,11 +79,26 @@ var App = React.createClass({
     onReload: function () {
         window.alert('TODO');
     },
+    onImportUrls: function () {
+        this.setState({importingUrls: true});
+    },
+    doImportUrls: function (urls) {
+        this.setState({importingUrls: false, urls: urls});
+    },
+    cancelImportUrls: function () {
+        this.setState({importingUrls: false});
+    },
     previousEnabled: function () {
         return this.state.idx > 0;
     },
     nextEnabled: function () {
-        return this.state.idx < (this.props.urls.length - 1);
+        return this.state.idx < (this.state.urls.length - 1);
+    },
+    reloadEnabled: function () {
+        return Boolean(this.currentUrl());
+    },
+    exportEnabled: function () {
+        return Boolean(this.currentUrl());
     },
     onLabelStartEdit: function (event) {
         this.setState({labelAt: event.data});
@@ -172,10 +204,37 @@ var Label = React.createClass({
     }
 });
 
+var UrlsImport = React.createClass({
+    getInitialState: function () {
+        return {urls_text: this.props.urls.join('\n')};
+    },
+    render: function () {
+        // TODO - this should be a dialog
+        return <div>
+            <textarea name="urls" onChange={ this.updateUrls }>
+                { this.state.urls_text }</textarea>
+            <a className="waves-effect waves-light btn"
+                onClick={ this.onCancel }>cancel</a>{ ' ' }
+            <a className="waves-effect waves-light btn"
+                onClick={ this.onImport }>import urls</a>
+        </div>;
+    },
+    updateUrls: function (event) {
+        this.setState({urls_text: event.target.value});
+    },
+    onImport: function (event) {
+        event.preventDefault();
+        this.props.doImportUrls(this.state.urls_text.split('\n'));
+    },
+    onCancel: function (event) {
+        event.preventDefault();
+        this.props.cancelImportUrls();
+    }
+});
+
 var labels = ['Title', 'Body', 'Author', 'Date'];
-var urls = ['http://risk.ru', 'http://google.com', 'http://twitter.com'];
 
 ReactDOM.render(
-    <App urls={ urls } labels={ labels }/>,
+    <App labels={ labels }/>,
     document.getElementById('app')
 );
