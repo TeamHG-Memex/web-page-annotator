b49e86d881d9a40a97d0499feac65e95a5caa823
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-08-23 17:10:03 +0200

    Rm unused imports

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import json
 import logging
 from pathlib import Path
 from typing import Dict
-from urllib.parse import urlsplit, urljoin, unquote, urlencode
 
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
